dd515de8b3c7014fb92009db37d03aa67a672673
RYOMA <tryoma0326@gmail.com>
2019-12-10 13:06:59 +0000

    6.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
- root 'static_pages#top'
- get '/signup', to:'users#new'
- resources :users
+  get 'sessions/new'
+
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+
+  # ログイン機能
+  post   '/login', to: 'sessions#create'
+  get    '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
+
 end
