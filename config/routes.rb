00f13253ffdac77df645537660a870a93ba13ee1
RYOMA <tryoma0326@gmail.com>
2019-12-21 13:08:20 +0000

    Finish add-user-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   # ログイン機能
-  get   '/login', to: 'sessions#new'
-  post    '/login', to: 'sessions#create'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   resources :users do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
     end
+    resources :attendances, only: :update # この行を追加します。
   end
 end
